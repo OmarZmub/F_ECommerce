6942f5915e7cbb6c52f1ec35c2a04285370053a5
OmarMubarak <omarzinhom25@gmail.com>
2025-05-14 01:56:49 +0300

    images

--- F_ECommerce.WebUI/obj/Debug/net8.0/F_ECommerce.WebUI.AssemblyInfo.cs
+++ F_ECommerce.WebUI/obj/Debug/net8.0/F_ECommerce.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F_ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199278fdcd9c482445fd7e5a4f7aee1aa47eb8b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("F_ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F_ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
